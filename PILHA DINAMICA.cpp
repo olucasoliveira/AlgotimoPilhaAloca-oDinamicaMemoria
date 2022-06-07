ee67b9c85e46f5e4b1cc437e2a29bc67e5cf4278
Lucas de Oliveira <91337785+olucasoliveira@users.noreply.github.com>
2022-06-07 13:41:15 -0300

    Update PILHA DINAMICA.cpp

--- PILHA DINAMICA.cpp
+++ PILHA DINAMICA.cpp
@@ -26,11 +26,11 @@ int pop(){
 	if(topo == NULL){
 		printf("\nA Pilha esta vazia\n\nInsira algum elemento primeiro\n");
 	}else{
-	aux = topo;
-	removido = topo->valor;
-	topo = topo->prox;
-	printf("\nO valor %d foi removido da pilha\n\n", removido);
-    free(aux);
+		aux = topo;
+		removido = topo->valor;
+		topo = topo->prox;
+		printf("\nO valor %d foi removido da pilha\n\n", removido);
+    		free(aux);
 	}
 }
 
@@ -39,15 +39,15 @@ void show(){
 	if(topo == NULL){
 		printf("\nA Pilha esta vazia\n\nInsira algum elemento primeiro\n");
 	}else{
-    aux = topo;
-    printf("\nOs valores inseridos na pilha sao:\n\n ");
-    while(aux != NULL)
-    {
-        printf("\n      %d ", aux->valor);
-        aux = aux->prox;
-    }
-	}
-    printf("\n\n");
+	    aux = topo;
+	    printf("\nOs valores inseridos na pilha sao:\n\n ");
+	    while(aux != NULL)
+	    {
+		printf("\n      %d ", aux->valor);
+		aux = aux->prox;
+	    }
+		}
+	 printf("\n\n");
 
 	return;
 }
@@ -58,7 +58,7 @@ int main(){
 	int control, quantPush, quantPop, inserted=0, removed=0;
 	
 	do{		
-		printf("\nEscolha uma opção: \n\n [1] Inserir  \n [2] Retirar \n [3] Imprimir \n [4] Sair \n\nA opção escolhida é: ");
+		printf("\nEscolha uma opÃ§Ã£o: \n\n [1] Inserir  \n [2] Retirar \n [3] Imprimir \n [4] Sair \n\nA opÃ§Ã£o escolhida Ã©: ");
 		scanf("%d", &control);
 		
 		if(control == 1)
@@ -74,13 +74,18 @@ int main(){
 		
 		if(control == 2)
 		{
-			printf("\nDigite a quantidade de elementos que deseja remover: ");
-			scanf("%d", &quantPop);
-			while( removed < quantPop){
-				pop();
-				removed++;
+			if(topo == NULL){
+				printf("\nA pilha esta vazia, ensina um elemento primeiro");
+			
+			}else{
+				printf("\nDigite a quantidade de elementos que deseja remover: ");
+				scanf("%d", &quantPop);
+				while( removed < quantPop){
+					pop();
+					removed++;
+				}
+				removed = 0;
 			}
-			removed = 0;
 		}	
 		
 		if(control == 3)
@@ -95,7 +100,7 @@ int main(){
 		
 	}while( control != 0 );
 	
-	printf("\nVocê saiu do programa");
+	printf("\nVocÃª saiu do programa");
 	
 
 	return 0;
